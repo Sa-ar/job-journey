a5ebeafa1297eb8ebd54af4ea81995d8df3599ab
Saar Davidson <davidsonsaar@gmail.com>
2023-06-05 02:39:39 +0300

    fix CSS bug

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { Icons } from "@/components/icons";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between h-10 max-w-4xl p-10 mx-auto">
+    <header className="flex items-center justify-between w-screen max-w-4xl px-10 mx-auto my-10">
       <h3 className="flex items-center flex-1 h-full font-semibold text-primary">
         <Icons.logo />
         JobJourney
