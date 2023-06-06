cc2ad26c4a5a541a1a34e21f20d959e0e9f3cd1b
Saar Davidson <davidsonsaar@gmail.com>
2023-06-07 00:10:04 +0300

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import Header from "@/components/header";
-import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
+import Header from "@/components/header";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
